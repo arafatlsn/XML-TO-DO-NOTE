fc538f3c94d6fc4479cba3e3a0c13fb69f145b60
Arafat37 <arafatsarkar22@gmail.com>
2022-07-30 21:01:42 +0600

    first commit

---
+++ src/COMPONENTS/HeaderComp.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './HeaderComp.css'
+
+const HeaderComp = () => {
+  return (
+    <div className='navbar-container'>
+      <div className='navbar-main-div'>
+        <h1>XML To-Do Note</h1>
+      </div>
+    </div>
+  );
+};
+
+export default HeaderComp;
---
+++ src/COMPONENTS/MainSection.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './MainSection.css'
+
+const MainSection = () => {
+  return (
+    <div className='main-section-container'>
+      
+    </div>
+  );
+};
+
+export default MainSection;
